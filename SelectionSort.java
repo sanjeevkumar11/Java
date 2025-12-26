fce49572815bd993a39c3535b34460878a0666e5
sanjeevkumar11 <josejohnson79.jj@gmail.com>
2025-12-26 11:03:23 +0530

    Selectionsort

---
+++ SelectionSort.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        for(int i=0;i<n;i++){
+            int mindex=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[j] < arr[mindex]){
+                    mindex = j;
+                }
+            }
+            int temp = arr[mindex];
+            arr[mindex] = arr[i];
+            arr[i] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
