52a4d693a5884270767395c84e63cf559939525b
sanjeevkumar11 <josejohnson79.jj@gmail.com>
2025-12-26 12:05:33 +0530

    two sorting

---
+++ InsertionSort.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j>0;j--){
+                if(arr[j] < arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+                else{
+                    break;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
